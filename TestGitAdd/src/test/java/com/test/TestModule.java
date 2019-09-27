dad96a8e5c52b82c9a249478b26ff06b21745b65
GitHnd04 <2892810776@qq.com>
2019-09-27 11:09:08 +0800

    提交测试类

---
+++ TestGitAdd/src/test/java/com/test/TestModule.java
@@ -0,0 +1,15 @@
+package com.test;
+
+
+import com.testGit.TestGitAddApp;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TestGitAddApp.class)
+@Slf4j
+public class TestModule {
+
+}
